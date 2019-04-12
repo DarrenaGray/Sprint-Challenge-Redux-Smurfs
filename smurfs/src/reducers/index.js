8017ea39bcfb1918b9963339ffc7942789eca66c
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-12 12:08:19 -0400

    Added reducer for fetching smurf data from api

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,46 @@
+import { FETCH_SMURFS_START, FETCH_SMURFS_SUCCESS, FETCH_SMURFS_FAILURE } from "../actions";
+
 /*
   Be sure to import in all of the action types from `../actions`
 */
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+ */
+ const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case FETCH_SMURFS_START:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: ''
+      }
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.paylod,
+        fetchingSmurfs: false,
+        error: ''
+      }
+    case FETCH_SMURFS_FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      }
+    default:
+      return state
+  }
+
+};
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +49,4 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export default reducer;
